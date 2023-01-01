c0d69b1d391e5ff8636b0c168aa9e7cf5ce89b33
Flavio Mendes <flaviojmendes@gmail.com>
2023-01-01 16:32:48 +0000

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+type Props = {
+    width: string;
+    handleButtonPress: (key: string) => void;
+    char: string;
+}
+
+export default function Button(props: Props) {
+  return (
+    <div
+      onClick={() => props.handleButtonPress(props.char)}
+      className={`w-1/5 cursor-pointer flex border-2 font-semibold text-2xl border-gray-700 hover:bg-gray-100 active:bg-gray-400`}
+    >
+      <span className="m-auto select-none">{props.char}</span>
+    </div>
+  );
+}
